6867bdd5c6b2e442a3490da9a349a5207e56688f
herding-wolf <herding.wolf.lc@gmail.com>
2020-05-06 09:22:06 +0800

    no message

--- 11-json.go
+++ 11-json.go
@@ -3,48 +3,31 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 )
 
-//Student 学生
-type Student struct {
-	ID     int
-	Gender string
-	Name   string
-}
-
-//Class 班级
-type Class struct {
-	Title    string
-	Students []*Student
-}
+//结构体中字段大写开头表示可公开访问，小写表示私有（仅在定义当前结构体的包中可访问）。
 
 func main() {
-	c := &Class{
-		Title:    "101",
-		Students: make([]*Student, 0, 200),
+	type Studen struct {
+		Name   string
+		Age    int
+		Gender int
 	}
-	for i := 0; i < 10; i++ {
-		stu := &Student{
-			Name:   fmt.Sprintf("stu%02d", i),
-			Gender: "男",
-			ID:     i,
-		}
-		c.Students = append(c.Students, stu)
+	type Class struct {
+		Title   string
+		Count   int
+		Studens []*Studen
 	}
-	//JSON序列化：结构体-->JSON格式的字符串
-	data, err := json.Marshal(c)
-	if err != nil {
-		fmt.Println("json marshal failed")
-		return
+	newClass := &Class{Title: "九年级", Count: 20, Studens: make([]*Studen, 0, 20)}
+	for i := 0; i < 20; i++ {
+		data := &Studen{Age: i, Name: "黄裕辉" + strconv.Itoa(i), Gender: i % 2}
+		newClass.Studens = append(newClass.Studens, data)
 	}
-	fmt.Printf("json:%s\n", data)
-	//JSON反序列化：JSON格式的字符串-->结构体
-	str := `{"Title":"101","Students":[{"ID":0,"Gender":"男","Name":"stu00"},{"ID":1,"Gender":"男","Name":"stu01"},{"ID":2,"Gender":"男","Name":"stu02"},{"ID":3,"Gender":"男","Name":"stu03"},{"ID":4,"Gender":"男","Name":"stu04"},{"ID":5,"Gender":"男","Name":"stu05"},{"ID":6,"Gender":"男","Name":"stu06"},{"ID":7,"Gender":"男","Name":"stu07"},{"ID":8,"Gender":"男","Name":"stu08"},{"ID":9,"Gender":"男","Name":"stu09"}]}`
-	c1 := &Class{}
-	err = json.Unmarshal([]byte(str), c1)
-	if err != nil {
-		fmt.Println("json unmarshal failed!")
-		return
-	}
-	fmt.Printf("%#v\n", c1)
+	// json 序列化
+	data, _ := json.Marshal(newClass)
+	// json 反序列化
+	n1 := &Class{}
+	json.Unmarshal([]byte(data), n1)
+	fmt.Printf("%s\n", data)
 }
